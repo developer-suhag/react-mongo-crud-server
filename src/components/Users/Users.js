53b5dc5cf665d698ec875d697a569b5216f4873f
Suhag Al Amin <developersuhag@gmail.com>
2021-10-22 14:12:43 +0600

    delete user

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -7,6 +7,25 @@ const Users = () => {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const handleDeleteUser = (id) => {
+    const proceed = window.confirm("Are you sure, you want to delete?");
+    if (proceed) {
+      const url = `http://localhost:5000/users/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert("delete successfuly");
+            const remaingingUsers = users.filter((user) => user._id != id);
+            setUsers(remaingingUsers);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h2>Users found: {users.length}</h2>
@@ -14,6 +33,8 @@ const Users = () => {
         {users.map((user) => (
           <li key={user._id}>
             Name: {user.name} :: Email: {user.email}
+            <button> 🖊️ </button>
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
           </li>
         ))}
       </ul>
