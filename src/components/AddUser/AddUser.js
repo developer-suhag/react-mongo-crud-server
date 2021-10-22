f432c413d54102cbab43f6e4ca2c4a316695de25
Suhag Al Amin <developersuhag@gmail.com>
2021-10-22 13:07:42 +0600

    add user

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -6,8 +6,8 @@ const AddUser = () => {
 
   const handleAddUser = (e) => {
     e.preventDefault();
-    const name = nameRef.current.value;
-    const email = emailRef.current.value;
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
     const newUser = { name, email };
     fetch("http://localhost:5000/users", {
       method: "POST",
@@ -15,14 +15,21 @@ const AddUser = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(newUser),
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert("succesfully add user");
+          e.target.reset();
+        }
+      });
   };
   return (
     <div>
       <h2>Please Add User</h2>
       <form onSubmit={handleAddUser}>
-        <input type="text" placeholder="name" />
-        <input type="email" placeholder="email" />
+        <input type="text" ref={nameRef} placeholder="name" />
+        <input type="email" ref={emailRef} placeholder="email" />
         <input type="submit" value="Add" />
       </form>
     </div>
